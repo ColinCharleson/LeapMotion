c4c86ee6dc2231bb2787c522ee5f8f470432775a
seifhelaly <seifhelaly@gmail.com>
2022-11-15 15:46:51 -0500

    Music

---
+++ LeapMotionGame/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource BackgroundMusic;
+
+    private float musicVolume = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        BackgroundMusic.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        BackgroundMusic.volume = musicVolume;
+
+
+    }
+
+
+}
